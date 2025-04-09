1bca21e0c6a66016426e5fca54d6286367b8a7b8
VlaArt2008 <vasanabria@poligran.edu.co>
2025-04-08 19:06:57 -0500

    Update Vehiculos.java

--- TrabajoColecciones/src/co/edu/poli/paradigmas/tc/entities/Vehiculos.java
+++ TrabajoColecciones/src/co/edu/poli/paradigmas/tc/entities/Vehiculos.java
@@ -1,1 +1,9 @@
 package co.edu.poli.paradigmas.tc.entities;
+public class Vehiculo {
+    private String placa;
+    private String modelo;
+    private int capacidad;
+    private String estado;
+
+    // Constructor, Getters, Setters
+}

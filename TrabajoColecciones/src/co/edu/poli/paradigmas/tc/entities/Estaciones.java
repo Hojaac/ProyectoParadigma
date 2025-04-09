be91b0ce7eb462259c6f80d5025d06f3142734b3
VlaArt2008 <vasanabria@poligran.edu.co>
2025-04-08 19:01:43 -0500

    Update Estaciones.java

--- TrabajoColecciones/src/co/edu/poli/paradigmas/tc/entities/Estaciones.java
+++ TrabajoColecciones/src/co/edu/poli/paradigmas/tc/entities/Estaciones.java
@@ -1,36 +1,11 @@
 package co.edu.poli.paradigmas.tc.entities;
 
-public class TipoDocumento {
-	
-	private int id;
-	
-	private String nombre;
+public class Estacion {
+    private int idEstacion;
+    private String nombre;
+    private int orden;
+    private int idRuta;
 
-	public TipoDocumento() {
-		super();
-	}
-
-	public TipoDocumento(int id, String nombre) {
-		super();
-		this.id = id;
-		this.nombre = nombre;
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getNombre() {
-		return nombre;
-	}
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-	
-
+    // Constructor, Getters, Setters
 }
+	

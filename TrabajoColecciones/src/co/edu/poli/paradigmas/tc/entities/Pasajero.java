661df4c8e874b1f39dc181c1bafb94027db165a3
VlaArt2008 <vasanabria@poligran.edu.co>
2025-04-08 19:05:47 -0500

    Update and rename Telefono.java to Pasajero.java

---
+++ TrabajoColecciones/src/co/edu/poli/paradigmas/tc/entities/Pasajero.java
@@ -0,0 +1,9 @@
+package co.edu.poli.paradigmas.tc.entities;
+
+public class Conductor {
+    private int idConductor;
+    private String nombre;
+    private String licencia;
+
+    // Constructor, Getters, Setters
+}

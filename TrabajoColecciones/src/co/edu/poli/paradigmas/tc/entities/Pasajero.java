d40a06044722ab776cacfbe89da5f0eedd68f70e
VlaArt2008 <vasanabria@poligran.edu.co>
2025-04-08 19:06:23 -0500

    Update Pasajero.java

--- TrabajoColecciones/src/co/edu/poli/paradigmas/tc/entities/Pasajero.java
+++ TrabajoColecciones/src/co/edu/poli/paradigmas/tc/entities/Pasajero.java
@@ -1,9 +1,2 @@
 package co.edu.poli.paradigmas.tc.entities;
 
-public class Conductor {
-    private int idConductor;
-    private String nombre;
-    private String licencia;
-
-    // Constructor, Getters, Setters
-}

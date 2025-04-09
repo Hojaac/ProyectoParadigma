17dee1c00cab284877843dd9cb0e4fb705ab983d
VlaArt2008 <vasanabria@poligran.edu.co>
2025-04-08 19:01:14 -0500

    Update Ruta.java

--- TrabajoColecciones/src/co/edu/poli/paradigmas/tc/entities/Ruta.java
+++ TrabajoColecciones/src/co/edu/poli/paradigmas/tc/entities/Ruta.java
@@ -1,38 +1,11 @@
 package co.edu.poli.paradigmas.tc.entities;
 
-public class Email {
-	
-	private int id;
-	
-	private String cuenta;
-	
+public class Ruta {
+    private int idRuta;
+    private String origen;
+    private String destino;
+    private double distancia;
+    private String horarios;
 
-	public Email() {
-		super();
-	}
-
-	public Email(int id, String cuenta) {
-		super();
-		this.id = id;
-		this.cuenta = cuenta;
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getCuenta() {
-		return cuenta;
-	}
-
-	public void setCuenta(String cuenta) {
-		this.cuenta = cuenta;
-	}
-	
-	
-
+    // Constructor, Getters, Setters
 }

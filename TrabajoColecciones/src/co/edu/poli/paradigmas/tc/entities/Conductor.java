24d9ad5952a9f3fa83bfa2fb0908f747e4e3319e
VlaArt2008 <vasanabria@poligran.edu.co>
2025-04-08 19:06:35 -0500

    Update Conductor.java

--- TrabajoColecciones/src/co/edu/poli/paradigmas/tc/entities/Conductor.java
+++ TrabajoColecciones/src/co/edu/poli/paradigmas/tc/entities/Conductor.java
@@ -1,1 +1,8 @@
 package co.edu.poli.paradigmas.tc.entities;
+public class Conductor {
+    private int idConductor;
+    private String nombre;
+    private String licencia;
+
+    // Constructor, Getters, Setters
+}
